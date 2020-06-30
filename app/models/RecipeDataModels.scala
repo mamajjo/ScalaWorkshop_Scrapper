52f677946df08ebd8144fab4d0c9a5b760189af7
Maciej Majchrowski <maciek21197@gmail.com>
2020-07-01 00:00:37 +0200

    feat

--- app/models/RecipeDataModels.scala
+++ app/models/RecipeDataModels.scala
@@ -27,6 +27,15 @@ class ScrapedDataModel(val recipeList: ScraperRecipe[HTMLElementModel], val down
             else if (i < list.length )
               list.update(i, (i+1).toString -> (list(i)._2 + "|" + extracted(i)))
           }
+        case HTMLElementModel(cssTag, cssValue, dataType) if cssTag=="span" && dataType == "text" =>
+          val tagValue = "." + tags.cssValue
+          val extracted = (downloadedDocument >> texts(tagValue)).toList
+          for (i <- extracted.indices) {
+            if (i == list.length)
+              list.append((i+1).toString -> extracted(i))
+            else if (i < list.length )
+              list.update(i, (i+1).toString -> (list(i)._2 + "|" + extracted(i)))
+          }
         case _ => throw DeserializationException("Unmet cssTag or dataType to extract in config JSON")
       }
     })
